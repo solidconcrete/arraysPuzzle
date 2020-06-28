ed43c9d930d44599ffddbc36d676a0c2824e1467
solidconcrete <saniasania523@gmail.com>
2020-06-28 17:57:40 +0300

    what

---
+++ futureProsHW/IO.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace futureProsHW
+{
+    class UI
+    {
+        public static void showMainScreen()
+       {
+            Console.WriteLine("Hello, waiting for an array of numbers... (ex. [1,5,3,0,1][1,0,0,5])");
+            string input = Console.ReadLine();
+            Console.WriteLine();
+            if (input == "history")
+            {
+                showHistory();
+            }
+            else
+            {
+                processArrays(input);
+            }
+            showMainScreen();
+        }
+        public static void processArrays(string input)
+        {
+            List<int[]> arrays = new List<int[]>();
+            var mathes = Regex.Matches(input, @"\[(.*?)\]");
+            foreach (Match m in mathes)
+            {
+                try
+                {
+                    arrays.Add(Array.ConvertAll(m.Groups[1].Value.Split(','), int.Parse));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine("bad input");
+                }
+
+            }
+            searchInHistory(arrays);
+        }
+        public static void sendToSolve(List<int[]> arrays)
+        {
+            Experiment experiment = new Experiment();
+            List<Move> moves = new List<Move>();
+            List<Move> result = new List<Move>();
+            //cycling through given arrays
+            foreach (var arr in arrays)
+            {
+                result = experiment.solveProblem(arr, 0, 1, moves);
+                Console.Write("[ ");
+                //cycling through elements of each array
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+
+                writeToFile(string.Join(',', arr));
+                Console.WriteLine("]");
+                showResults(result);
+                experiment.cleanBest();
+            }
+        }
+        public static void showResults(List<Move> result)
+        {
+            if (result.Count == 0)
+            {
+                Console.WriteLine("No path found =(");
+                writeToFile("no path found");
+            }
+            else
+            {
+                foreach (var res in result)
+                {
+                    Console.WriteLine(res);
+                    writeToFile(res.ToString());
+                }
+            }
+            Console.WriteLine();
+        }
+        public static void writeToFile(string text)
+        {
+
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            var txtPath = Path.Combine(path, "futureProsData.txt");
+
+            if (!File.Exists(txtPath))
+            {
+                using (StreamWriter sw = File.CreateText(txtPath));
+            }
+            using (StreamWriter sw = File.AppendText (txtPath))
+            {
+                sw.WriteLine(text);
+            }
+            
+        }
+        private static void searchInHistory(List<int[]> arrays)
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            var txtPath = Path.Combine(path, "futureProsData.txt");
+
+            bool found = false;
+            List<int[]> notFound = new List<int[]>();
+
+            List<string> history = readFile();
+            foreach (var arr in arrays)
+            {
+                if(arr.Length < 2)
+                {
+                    Console.WriteLine("array too short");
+                    continue;
+                }
+                notFound.Add(arr);                                                      //user given aray not found in history so far
+                foreach (var his in history)
+                {
+                    if (his == string.Join(',', arr))
+                    {                        
+                        Console.WriteLine('[' + his + "] found in history");                        //if array found in history, its removed from notFound
+                        notFound.Remove(arr);
+                        found = true;
+                        continue;
+                    }
+                    if (found == true && !Char.IsDigit(his[0]))                             //only steps in history start with letters
+                    {
+                        Console.WriteLine(his);                       
+                    }
+                    if (found == true && Char.IsDigit(his[0]))                              //only array combinations start with digits in history
+                    {
+                        found = false;
+                        continue;
+                    }
+                    
+                }
+            }
+            sendToSolve(notFound);                                                          //arrays not found in history are sent to be solved
+                
+        }
+        private static List<string> readFile()
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            var txtPath = Path.Combine(path, "futureProsData.txt");
+            List<string> history = new List<string>();
+            using (StreamReader sr = new StreamReader(txtPath))
+            {
+                string line;             
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                    history.Add(line);
+                    }                
+            }
+            return history;
+        }
+        private static void showHistory()
+        {
+            List<string> history = readFile();
+
+            foreach (var his in history)
+            {
+                Console.WriteLine(his);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ futureProsHW/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace futureProsHW
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string input = "2, 3, 1, 2, 10, 5, 2, -1, 1, 1, 2, 0, 1, 0 ";
+            //string input = "1, 2, 0, 0, 1, 2, 0";
+            //string input = "1, 2, 0, 1, 3, 2, 0";
+
+            UI.showMainScreen();
+            
+        }
+
+    }
+    class Experiment
+    {
+        List<Move> bestPath = new List<Move>();
+        public List<Move> solveProblem(int[] array, int currentPosition, int steps, List<Move> moves)
+        {
+            //this if is triggered when finish is reached. It checks whether the path is more efficient than the current most efficient path
+            if (currentPosition >= array.Length - 1)
+            {  
+                if(bestPath.Count == 0 || moves.Count < bestPath.Count)
+                {
+                    bestPath.Clear();
+                    foreach (var move in moves)
+                    {
+                        bestPath.Add(move);
+                    }
+                    return moves;
+                }
+            }
+            else
+                for (int i = array[currentPosition]; i > 0; i--)
+                {
+                    Move move = PathfindHelper.checkOutOfBounds(steps,array, currentPosition, i);
+                    moves = PathfindHelper.checkBacktrack(steps, moves, move);
+                    solveProblem(array, currentPosition + i, steps + 1, moves);   
+                }
+            return bestPath;
+        }
+        public  void cleanBest()
+        {
+            bestPath.Clear();               //before solving next array, old path must be removed
+        }
+    }
+
+    class Move
+    {
+        private int step, from, fromAt, to, toAt;
+        public Move(int step, int from, int fromAt, int to, int toAt)
+        {
+            this.step = step;
+            this.from = from;
+            this.fromAt = fromAt;
+            this.to = to;
+            this.toAt = toAt;
+        }
+
+        public override string ToString()
+        {
+            return ("Step " + step + ": from " + from + " at index " + fromAt + ",  to " + to + " at index " + toAt);
+        }
+    }
+
+    class PathfindHelper
+    {        
+        public static Move checkOutOfBounds(int steps, int[] array, int currentPosition, int i)
+        {
+            int to, toAt;
+            if (currentPosition + i < array.Length)
+
+            {
+                to = array[currentPosition + i];
+                toAt = currentPosition + i;                  
+            }
+            else
+            {
+                //prevents from going out of bounds
+                to = array[array.Length - 1];
+                toAt = array.Length - 1;
+            }
+            return new Move(steps, array[currentPosition], currentPosition, to, toAt);
+        }
+        public static List<Move> checkBacktrack(int steps, List<Move> moves, Move move)
+        {
+            if (moves.Count < steps)
+            {
+                moves.Add(move);
+            }
+            else
+            {
+                //if backtracking, remove old moves from list
+                moves.RemoveRange(steps - 1, moves.Count - steps + 1);
+                moves.Add(move);
+            }
+            return moves;
+        }
+    }
+}
